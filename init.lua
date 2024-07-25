9c4aeaf434a4393b58166d3b93a32cce4516dbab
SantoshChiniwar <chiniwarsp@gmail.com>
2024-07-25 19:35:24 +0800

    Update V1.

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("chipware.core")
 require("chipware.lazy")
 
+-- Set Neovim options
+vim.opt.relativenumber = false -- Enable relative line numbers
+vim.opt.number = true -- Enable absolute line numbers for the current line
+vim.opt.virtualedit:append("onemore") -- Set virtualedit+=onemore
+
+-- Set key mappings
+vim.api.nvim_set_keymap("n", "1", "^", { noremap = true, silent = true }) -- nnoremap 1 ^
+vim.api.nvim_set_keymap("n", "2", "g$", { noremap = true, silent = true }) -- nnoremap 2 g$
